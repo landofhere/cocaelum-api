b34d1bc3afa3a24bae500f4fd1fc3c2af8d27af9
Barry Low <barrylow@gmail.com>
2017-10-15 23:13:24 -0700

    Added GetWeatherByCity Schema

--- data/schema.js
+++ data/schema.js
@@ -1,13 +1,37 @@
 import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
+import GraphQLJSON from 'graphql-type-json';
 import mocks from './mocks';
 
 const typeDefs = `
+scalar JSON
+
+type GetWeatherByCityPayload {
+  name: String
+  dt: Float
+  main: JSON
+  sys: JSON
+  wind: JSON
+  base: String
+  owid: Int
+  coord: JSON
+  clouds: JSON
+  rain: JSON
+  snow: JSON
+  weather: [JSON]
+  visibility: Int
+  cod: Int
+}
+
 type Query {
-  testString: String
+  getWeatherByCity(city: String!): GetWeatherByCityPayload
 }
 `;
 
-const schema = makeExecutableSchema({ typeDefs });
+const resolveFunctions = {
+  JSON: GraphQLJSON
+};
+
+const schema = makeExecutableSchema({ typeDefs, resolvers: resolveFunctions });
 
 addMockFunctionsToSchema({ schema, mocks });
 
